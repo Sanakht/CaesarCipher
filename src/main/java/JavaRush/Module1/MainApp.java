dc72a33cc227e061b36f1d891dd3837c988cf8f2
Yaroslav Bogomolov <bogomolov200@yandex.ru>
2025-04-22 22:38:47 +0500

    Добавление метода шифрования

--- src/main/java/JavaRush/Module1/MainApp.java
+++ src/main/java/JavaRush/Module1/MainApp.java
@@ -1,9 +1,25 @@
 package JavaRush.Module1;
 
+import JavaRush.Module1.model.Cipher;
+
 public class MainApp
 {
+    private static final char[] ALPHABET = {'а', 'б', 'в', 'г', 'д', 'е', 'ж', 'з',
+            'и','к', 'л', 'м', 'н', 'о', 'п', 'р', 'с', 'т', 'у', 'ф', 'х', 'ц', 'ч', 'ш', 'щ',
+            'ъ', 'ы', 'ь', 'э', 'я', '.', ',', '«', '»', '"', '\'', ':', '!', '?', ' '};
+
+
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+       Cipher cipher = new Cipher(ALPHABET);
+
+       String text = "добавить массив";
+
+        System.out.println(cipher.encrypt(text, 5));
+
+//        StringBuilder stringBuilder = new StringBuilder();
+//        char[] chars = text.toCharArray();
+//        stringBuilder.append(text);
+//        System.out.println(stringBuilder);
     }
 }
--- src/main/java/JavaRush/Module1/model/Cipher.java
+++ src/main/java/JavaRush/Module1/model/Cipher.java
@@ -9,13 +9,28 @@ public class Cipher {
     }
 
     public String encrypt(String text, int shift) {
+
         char[] arrayChar = text.toCharArray();
 
         char[] newArrayChar = new char[arrayChar.length];
 
+        for (int i = 0; i < arrayChar.length; i++) {
+            for (int j = 0; j < alphabet.length; j++) {
 
+                if(arrayChar[i] == alphabet[j]) {
+                   int index = j + shift;
+                   if(index > alphabet.length - 1 ) {
+                       index = index - alphabet.length;
+                       newArrayChar[i] = alphabet[index];
+                   }
+                   newArrayChar[i] = alphabet[index];
+                }
+            }
+        }
 
-        return "";
+        StringBuilder str = new StringBuilder();
+        str.append(newArrayChar);
+        return str.toString();
     }
     public String decrypt(String encryptedText, int shift) {
         // Логика расшифровки
