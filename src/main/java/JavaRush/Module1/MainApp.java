0d057f50db4cfe4797376ce6feca57634e606b03
Yaroslav Bogomolov <bogomolov200@yandex.ru>
2025-04-23 00:12:18 +0500

    Добавление метода шифрования(считывание файла с компьютера и создание нового файла)

--- src/main/java/JavaRush/Module1/MainApp.java
+++ src/main/java/JavaRush/Module1/MainApp.java
@@ -2,6 +2,9 @@ package JavaRush.Module1;
 
 import JavaRush.Module1.model.Cipher;
 
+import java.io.IOException;
+import java.nio.file.Path;
+
 public class MainApp
 {
     private static final char[] ALPHABET = {'а', 'б', 'в', 'г', 'д', 'е', 'ж', 'з',
@@ -11,15 +14,22 @@ public class MainApp
 
     public static void main( String[] args )
     {
-       Cipher cipher = new Cipher(ALPHABET);
+        Cipher cipher = new Cipher(ALPHABET);
 
-       String text = "добавить массив";
+        String inputFile = "C:\\Users\\User\\Desktop\\TestModule1\\Text.txt";
 
-        System.out.println(cipher.encrypt(text, 5));
+        String outPutFile = "C:\\Users\\User\\Desktop\\TestModule1\\Text2.txt";
 
-//        StringBuilder stringBuilder = new StringBuilder();
-//        char[] chars = text.toCharArray();
-//        stringBuilder.append(text);
-//        System.out.println(stringBuilder);
+//        Path inputFile = Path.of("C:\\Users\\User\\Desktop\\TestModule1\\Text.txt");
+//        Path outPutFile = Path.of("C:\\Users\\User\\Desktop\\TestModule1\\Text2.txt");
+
+
+        try {
+            cipher.encrypt(inputFile,outPutFile,5);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+
     }
 }
--- src/main/java/JavaRush/Module1/model/Cipher.java
+++ src/main/java/JavaRush/Module1/model/Cipher.java
@@ -1,5 +1,12 @@
 package JavaRush.Module1.model;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Cipher {
 
     private char[] alphabet;
@@ -8,29 +15,47 @@ public class Cipher {
         this.alphabet = alphabet;
     }
 
-    public String encrypt(String text, int shift) {
+    public void encrypt(String inputFile, String outputFile, int key) throws IOException {
+        StringBuilder str = new StringBuilder();
 
-        char[] arrayChar = text.toCharArray();
+        Path inputPath = Paths.get(inputFile);
+        Path outputPath = Paths.get(outputFile);
 
-        char[] newArrayChar = new char[arrayChar.length];
+        List<String> listInputFile = Files.readAllLines(inputPath);
 
-        for (int i = 0; i < arrayChar.length; i++) {
-            for (int j = 0; j < alphabet.length; j++) {
+        for(String text: listInputFile) {
+            char[] arrayChar = text.toCharArray();
 
-                if(arrayChar[i] == alphabet[j]) {
-                   int index = j + shift;
-                   if(index > alphabet.length - 1 ) {
-                       index = index - alphabet.length;
-                       newArrayChar[i] = alphabet[index];
-                   }
-                   newArrayChar[i] = alphabet[index];
+            char[] newArrayChar = new char[arrayChar.length];
+
+            for (int i = 0; i < arrayChar.length; i++) {
+                for (int j = 0; j < alphabet.length; j++) {
+
+                    if(arrayChar[i] == alphabet[j]) {
+                        int index = j + key;
+                        if(index > alphabet.length - 1 ) {
+                            index = index - alphabet.length;
+                            newArrayChar[i] = alphabet[index];
+                        }
+                        newArrayChar[i] = alphabet[index];
+                    }
                 }
             }
+
+            str.append(newArrayChar).append("\n");
         }
 
-        StringBuilder str = new StringBuilder();
-        str.append(newArrayChar);
-        return str.toString();
+        if(Files.isRegularFile(outputPath)) {
+            Files.write(outputPath, str.toString().getBytes());
+        } else {
+            Files.createFile(outputPath);
+            Files.write(outputPath, str.toString().getBytes());
+        }
+
+
+
+
+        //return str.toString();
     }
     public String decrypt(String encryptedText, int shift) {
         // Логика расшифровки
